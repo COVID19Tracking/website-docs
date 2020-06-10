c9746b882cd6baa9495e762c71f1d4ca38d9ce0b
COVID Tracking Project <accounts@covidtracking.com>
2020-06-10 16:24:17 +0000

    Updates

--- react-syntax-highlighter_languages_highlight_erb.e0d601874357d1eee025.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{1846:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[hljs.COMMENT("<%#","%>"),{begin:"<%[%=-]?",end:"[%-]?%>",subLanguage:"ruby",excludeBegin:!0,excludeEnd:!0}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_erb.e0d601874357d1eee025.bundle.js.map
---
+++ react-syntax-highlighter_languages_highlight_plaintext.383d4bde4ddb31fc1577.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[121],{1919:function(module,exports){module.exports=function(hljs){return{disableAutodetect:!0}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_plaintext.383d4bde4ddb31fc1577.bundle.js.map
