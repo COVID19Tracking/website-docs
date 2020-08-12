e9cfb1edfa5be9f33f0d1a0c71adc3e35be0efe4
COVID Tracking Project <accounts@covidtracking.com>
2020-08-12 20:02:21 +0000

    Updates

--- react-syntax-highlighter_languages_highlight_clojureRepl.288adfeefb14ac6c3f98.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{1850:function(module,exports){module.exports=function(hljs){return{contains:[{className:"meta",begin:/^([\w.-]+|\s*#_)=>/,starts:{end:/$/,subLanguage:"clojure"}}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_clojureRepl.288adfeefb14ac6c3f98.bundle.js.map
---
+++ react-syntax-highlighter_languages_highlight_plaintext.7493af8ac157f6e92265.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[121],{1949:function(module,exports){module.exports=function(hljs){return{disableAutodetect:!0}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_plaintext.7493af8ac157f6e92265.bundle.js.map
