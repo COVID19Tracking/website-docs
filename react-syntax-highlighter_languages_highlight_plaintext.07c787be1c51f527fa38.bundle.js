df98e72002fafa92b7c4b24724ac68927fd85659
COVID Tracking Project <accounts@covidtracking.com>
2020-09-11 20:50:56 +0000

    Updates

--- react-syntax-highlighter_languages_highlight_plaintext.07c787be1c51f527fa38.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[121],{1949:function(module,exports){module.exports=function(hljs){return{disableAutodetect:!0}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_plaintext.07c787be1c51f527fa38.bundle.js.map
--- react-syntax-highlighter_languages_highlight_shell.07c787be1c51f527fa38.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[148],{1976:function(module,exports){module.exports=function(hljs){return{aliases:["console"],contains:[{className:"meta",begin:"^\\s{0,3}[\\w\\d\\[\\]()@-]*[>%$#]",starts:{end:"$",subLanguage:"bash"}}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_shell.07c787be1c51f527fa38.bundle.js.map
